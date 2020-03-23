58e19376cff674ce500972631733349d63c24c83
Issac Moreno <issaxavier909@gmail.com>
2020-03-23 15:29:46 -0700

    added user model and user-router. auth-router still in progress

--- auth/router.js
+++ auth/router.js
@@ -3,3 +3,21 @@ const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 
 const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+    const userInfo = req.body;
+
+    const ROUNDS = process.env.HASH_ROUNDS || 8
+    const hash = bcrypt.hashSync(userInfo.password, ROUNDS)
+
+    userInfo.password = hash;
+
+    Users
+        .add(userInfo)
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -1,1 +1,29 @@
 const db = require("../database/dbConfig.js");
+
+module.exports = {
+    find,
+    findBy,
+    findById,
+    add
+};
+
+function find() {
+    return db('users').select('id', 'username');
+}
+
+function findBy(filter) {
+    return db('users').where(filter);
+}
+
+function findById(id) {
+    return db('users')
+        .where({id})
+        .select('id', 'username', 'password')
+        .first();
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user, 'id');
+
+    return findById(id);
+}
